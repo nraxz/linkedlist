d571fceff14178cb01599b27c87fae84b8fda82b
Naresh Shahi <thenraxz@gmail.com>
2019-02-28 16:09:18 +0000

    New linked list examples

---
+++ linkedlist2.php
@@ -0,0 +1,146 @@
+<?php
+
+/**
+ * Class Node
+ */
+class Node
+{
+    public $data;
+    public $next;
+
+    public function __construct($item)
+    {
+        $this->data = $item;
+        $this->next = null;
+    }
+}
+
+/**
+ * Class LinkList
+ */
+class LinkList
+{
+    public $head = null;
+
+    private static $count = 0;
+
+    /**
+     * @return int
+     */
+    public function GetCount()
+    {
+        return self::$count;
+    }
+
+    /**
+     * @param mixed $item
+     */
+    public function InsertAtFist($item) {
+        if ($this->head == null) {
+            $this->head = new Node($item);
+        } else {
+            $temp = new Node($item);
+
+            $temp->next = $this->head;
+
+            $this->head = $temp;
+        }
+
+        self::$count++;
+    }
+
+    /**
+     * @param mixed $item
+     */
+    public function InsertAtLast($item) {
+        if ($this->head == null) {
+            $this->head = new Node($item);
+        } else {
+            /** @var Node $current */
+            $current = $this->head;
+            while ($current->next != null)
+            {
+                $current = $current->next;
+            }
+
+            $current->next = new Node($item);
+        }
+
+        self::$count++;
+    }
+
+    /**
+     * Delete the node which value matched with provided key
+     * @param $key
+     */
+    public function Delete($key)
+    {
+        /** @var Node $current */
+        $current = $previous = $this->head;
+
+        while($current->data != $key) {
+            $previous = $current;
+            $current = $current->next;
+        }
+
+        // For the first node
+        if ($current == $previous) {
+            $this->head = $current->next;
+        }
+
+        $previous->next = $current->next;
+
+        self::$count--;
+    }
+
+    /**
+     * Print the link list as string like 1->3-> ...
+     */
+    public function PrintAsList()
+    {
+        $items = [];
+        /** @var Node $current */
+        $current = $this->head;
+        while($current != null) {
+            array_push($items, $current->data);
+            $current = $current->next;
+        }
+
+        $str = '';
+        foreach($items as $item)
+        {
+            $str .= $item . '->';
+        }
+
+        echo $str;
+
+        echo PHP_EOL;
+    }
+}
+
+$ll = new LinkList();
+
+$ll->InsertAtLast('KP');
+$ll->InsertAtLast(45);
+$ll->InsertAtFist(11);
+$ll->InsertAtLast('FE');
+$ll->InsertAtFist('LE');
+$ll->InsertAtFist(100);
+$ll->InsertAtFist(199);
+$ll->InsertAtLast(500);
+
+$ll->PrintAsList();
+echo 'Total elements ' . $ll->GetCount();
+echo PHP_EOL;
+$ll->Delete(45);
+$ll->PrintAsList();
+echo 'Total elements ' . $ll->GetCount();
+echo PHP_EOL;
+$ll->Delete(500);
+$ll->PrintAsList();
+echo 'Total elements ' . $ll->GetCount();
+echo PHP_EOL;
+$ll->Delete(100);
+$ll->PrintAsList();
+echo 'Total elements ' . $ll->GetCount();
+echo PHP_EOL;
